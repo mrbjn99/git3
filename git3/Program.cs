9d0afc3ad4b3c380aa4ada3dcd119ba717afe22a
mrbjn99 <58242504+mrbjn99@users.noreply.github.com>
2019-11-30 08:38:19 +0700

    Update Program.cs

--- git3/Program.cs
+++ git3/Program.cs
@@ -7,6 +7,7 @@ namespace git3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
